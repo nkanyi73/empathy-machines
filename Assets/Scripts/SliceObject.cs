71cb8e0876b01b1c9561c8e9fe54a521faf3bab4
nkanyi73 <nkanyi73@gmail.com>
2024-02-12 09:58:14 +0000

    Add scripts that allow for tomato slicing

---
+++ Assets/Scripts/SliceObject.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+using UnityEngine.InputSystem;
+using Oculus.Interaction;
+using System.Reflection;
+using Oculus.Interaction.HandGrab;
+
+public class SliceObject : MonoBehaviour
+{
+    public Transform startSlicePoint;
+    public Transform endSlicePoint;
+    public VelocityEstimator velocityEstimator;
+    public VelocityTracker velocityTracker;
+    public Material crossSectionMaterial;
+    public float cutForce;
+    public LayerMask sliceableLayer;
+    public GameObject grabbablePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool hasHit = Physics.Linecast(startSlicePoint.position, endSlicePoint.position, out RaycastHit hit, sliceableLayer);
+        if (hasHit)
+        {
+            if (velocityTracker.atChoppingSpeed)
+            {
+                GameObject target = hit.transform.gameObject;
+                Slice(target);
+                //Debug.Log(target);
+            }
+        }
+    }
+
+    public void Slice(GameObject target)
+    {
+        Vector3 velocity = velocityEstimator.GetVelocityEstimate();
+        Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity);
+        planeNormal.Normalize();
+
+        SlicedHull hull = target.Slice(endSlicePoint.position, planeNormal);
+
+        if (hull != null )
+        {
+            GameObject upperHull = hull.CreateUpperHull(target, crossSectionMaterial);
+            SetUpSlicedComponent(upperHull);
+
+            GameObject lowerHull = hull.CreateLowerHull(target, crossSectionMaterial);
+            SetUpSlicedComponent(lowerHull);
+
+
+
+            Destroy(target);
+        }
+    }
+
+    public void SetUpSlicedComponent(GameObject slicedGameObject)
+    {
+        //Rigibody
+        Rigidbody rb = slicedGameObject.AddComponent<Rigidbody>();
+        rb.AddExplosionForce(cutForce, slicedGameObject.transform.position, 0.0000001f);
+        // Coillider
+        MeshCollider collider = slicedGameObject.AddComponent<MeshCollider>();
+        collider.convex = true;
+
+        // Setting the Sliceable layer
+
+        slicedGameObject.layer = LayerMask.NameToLayer("Sliceable");
+
+
+        // Grababble
+        Grabbable gb = slicedGameObject.AddComponent<Grabbable>();
+        gb.TransferOnSecondSelection = true;
+
+        // HandGrabInteractable
+        HandGrabInteractable hg = slicedGameObject.AddComponent<HandGrabInteractable>();
+        hg.InjectRigidbody(rb);
+        hg.InjectSupportedGrabTypes(Oculus.Interaction.Grab.GrabTypeFlags.All);
+        hg.HandAlignment = HandAlignType.None;
+        hg.InjectOptionalPointableElement(gb);
+        //// PointableElement pe = slicedGameObject.AddComponent<PointableElement>();
+        //InteractableGroupView gv = slicedGameObject.AddComponent<InteractableGroupView>();
+        //List<IInteractableView> interactables = new List<IInteractableView>
+        //{
+        //    hg
+        //};
+        //gv.InjectInteractables(interactables);
+
+        // Pointable Unity Event Wrapper
+        //PointableUnityEventWrapper ew = slicedGameObject.AddComponent<PointableUnityEventWrapper>();
+        //ew.InjectPointable(gb);
+        // ////List<>
+        // ////gv.InjectInteractables(0) = gb;
+    }
+}
---
+++ Assets/Scripts/VelocityTracker.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityTracker : MonoBehaviour
+{
+    public float velocityThreshold; // Threshold velocity value
+    public float timeInterval; // Time interval for velocity calculation
+    private Vector3 previousPosition;
+    public bool isGrabbed;
+    public bool atChoppingSpeed;
+
+    void Start()
+    {
+        previousPosition = transform.position; // Initialize the previous position
+        //InvokeRepeating("CalculateVelocity", 0f, timeInterval); // Start the velocity calculation
+    }
+
+    private void Update()
+    {
+        if(isGrabbed)
+        {
+            CalculateVelocity();
+        }
+    }
+
+    void CalculateVelocity()
+    {
+       // previousPosition = transform.position; // Initialize the previous position
+
+        Vector3 currentPosition = transform.position;
+        Vector3 displacement = currentPosition - previousPosition;
+        float velocityMagnitude = displacement.magnitude / timeInterval;
+
+        if (velocityMagnitude >= velocityThreshold)
+        {
+            Debug.Log("Object velocity has reached the threshold: " + velocityMagnitude);
+            atChoppingSpeed = true;
+            // You can perform additional actions here if needed
+        }
+        else
+        {
+            atChoppingSpeed = false;
+        }
+
+        previousPosition = currentPosition; // Update the previous position
+
+    } 
+
+    public void PickedUpKnife()
+    {
+        isGrabbed = true;
+        Debug.Log("Knife Picked Up");
+    }
+
+    public void ReleasedKnife()
+    {
+        isGrabbed = false;
+        Debug.Log("Knife Dropped");
+
+    }
+}
+
+
