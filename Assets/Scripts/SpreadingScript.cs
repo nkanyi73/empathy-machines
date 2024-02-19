2056608e50ce58de583351dd58e03f9080fc968e
nkanyi73 <nkanyi73@gmail.com>
2024-02-19 10:58:10 +0000

    Update the spreading script to include Butter

--- Assets/Scripts/SpreadingScript.cs
+++ Assets/Scripts/SpreadingScript.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class SpreadingScript : MonoBehaviour
 {
     public GameObject mayoSphere;
+    public GameObject butterSphere;
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Mayo")
+        if(collision.gameObject.tag == "Mayo" && this.gameObject.tag == "Spoon")
         {
             mayoSphere.SetActive(true);
+        }
+
+        if (collision.gameObject.tag == "Butter" && this.gameObject.tag == "Butter Knife")
+        {
+            butterSphere.SetActive(true);
         }
     }
 }
